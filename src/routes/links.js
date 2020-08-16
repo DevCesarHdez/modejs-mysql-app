01e3a5ea656702ea9f18c00a8bc5a38ab0d6dc83
Cretrok <solosemeocurioesto@gmail.com>
2020-08-16 15:11:47 -0500

    proteccion de rutas

--- src/routes/links.js
+++ src/routes/links.js
@@ -2,12 +2,13 @@ const express = require('express');
 const router = express.Router();
 
 const pool = require('../database.js');
+const { isLoggedIn } = require('../lib/auth');
 
-router.get('/add', (req, res) => {
+router.get('/add', isLoggedIn, (req, res) => {
   res.render('links/add.hbs');
 })
 
-router.post('/add', async (req, res) => {
+router.post('/add', isLoggedIn, async (req, res) => {
   const { title, url, description } = req.body
   const newLink = {
     title,
@@ -20,27 +21,27 @@ router.post('/add', async (req, res) => {
   res.redirect('/links')
 });
 
-router.get('/', async (req, res) => {
+router.get('/', isLoggedIn, async (req, res) => {
   const links = await pool.query('SELECT * FROM links');
   //console.log(links);
   res.render('links/list.hbs', { links: links });
 })
 
-router.get('/delete/:id', async (req, res) => {
+router.get('/delete/:id', isLoggedIn, async (req, res) => {
   const {id} = req.params;
   await pool.query('DELETE FROM links WHERE id = ?', [id]);
   req.flash('success', 'link removed successfully');
   res.redirect('/links');
 });
 
-router.get('/edit/:id', async (req, res) => {
+router.get('/edit/:id', isLoggedIn, async (req, res) => {
   const { id } = req.params;
   const link = await pool.query('SELECT * FROM links WHERE id = ?', [id]);
   console.log(link[0]);
   res.render('links/edit.hbs',{link: link[0]});
 });
 
-router.post('/edit/:id', async (req, res) => {
+router.post('/edit/:id', isLoggedIn, async (req, res) => {
   const { id } = req.params;
   const { title, url, description} = req.body;
   const updateLink = {
